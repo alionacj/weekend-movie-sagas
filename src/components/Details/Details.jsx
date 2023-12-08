76627bbacb65df97cf52540dcf8db6084cc6509e
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-08 15:38:15 -0600

    created details page back button

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,9 @@
 import { useSelector } from "react-redux"
+import { useHistory } from "react-router-dom/cjs/react-router-dom"
 
 function Details() {
+
+    const history = useHistory()
 
     const detailsId = useSelector(store => store.detailsId)
     const movies = useSelector(store => store.movies)
@@ -13,11 +16,16 @@ function Details() {
         }
     }
 
+    const returnHome = () => {
+        history.push('/')
+    }
+
     return (
         <div>
             <h1>{selectedMovie.title}</h1>
             <img src={selectedMovie.poster} />
             <p>{selectedMovie.description}</p>
+            <button onClick={returnHome}>Back</button>
         </div>
     )
 }
