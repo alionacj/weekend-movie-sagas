60488c9be698e1e88c4fde8863f4742fbd1aac98
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-08 13:07:09 -0600

    created details component and navigation

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom'
 import './MovieList.css'
 
 function MovieList() {
 
-  const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
 
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
+
+  const history = useHistory()
+  const navToDetails = () => {
+    history.push('/details')
+  }
 
   return (
     <main>
@@ -19,7 +25,10 @@ function MovieList() {
           return (
             <div data-testid='movieItem' key={movie.id}>
               <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+              <img
+                src={movie.poster}
+                alt={movie.title}
+                onClick={navToDetails}/>
             </div>
           );
         })}
